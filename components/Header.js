8bbbe198e02663189ba41da1a854bfabc5b884da
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-03-08 09:27:55 -0800

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,57 @@
 import React from 'react'
 import Image from 'next/image';
+import {
+    BellIcon,
+    ChatIcon,
+    CheveronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
     return (
         <div>
-        <h1>Header</h1>
         {/* LEFT */}
         
-        <div>
-            <Image src="https://links.papareact.com/5me"
+        <div className="flex items-center">
+            <Image
+            src="https://links.papareact.com/5me"
             width={40}
             height={40}
             layout="fixed"
             />
             {/* input searchbar */}
-            <div>
-                
-                <input type="text" placeholder="Search Blazon" />
+            <div className="flex ml-2 items-center rounded-full
+            bg-gray-100 p-2">
+                <SearchIcon className="h-6 text-gray-600"
+                />
+                <input
+                    className='flex ml-2 items-center bg-transparent
+                    outline-none placeholder-gray-500 flex-shrink'
+                    type="text"
+                    placeholder="Search Blazon"
+                />
             </div>
         </div>
-        {/* CENTER */}
 
+        {/* CENTER */}
+        <div className="flex justify-center flex-grow">
+            <div className='flex space-x-6 md:space-x-2'>
+                <HeaderIcon Icon={HomeIcon} />
+                <HeaderIcon Icon={FlagIcon} />
+                <HeaderIcon Icon={PlayIcon} />
+                <HeaderIcon Icon={ShoppingCartIcon} />
+                <HeaderIcon Icon={UserGroupIcon} />
+            </div>
+        </div>
         {/* RIGHT */}
 
         </div>
