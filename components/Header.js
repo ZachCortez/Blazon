76ebc29dc9394f2770f05f0214acf242174117cf
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-03-09 21:15:38 -0800

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,8 @@ import HeaderIcon from "./HeaderIcon";
 
 function Header() {
     return (
-        <div>
+        <div className='sticky top-0 z-50 bg-white flex
+        items-center p-2 lg:px-5 shadow-md'>
         {/* LEFT */}
         
         <div className="flex items-center">
@@ -34,7 +35,7 @@ function Header() {
                 <SearchIcon className="h-6 text-gray-600"
                 />
                 <input
-                    className='flex ml-2 items-center bg-transparent
+                    className='hidden md:inline-flex ml-2 items-center bg-transparent
                     outline-none placeholder-gray-500 flex-shrink'
                     type="text"
                     placeholder="Search Blazon"
@@ -45,7 +46,7 @@ function Header() {
         {/* CENTER */}
         <div className="flex justify-center flex-grow">
             <div className='flex space-x-6 md:space-x-2'>
-                <HeaderIcon Icon={HomeIcon} />
+                <HeaderIcon active Icon={HomeIcon} />
                 <HeaderIcon Icon={FlagIcon} />
                 <HeaderIcon Icon={PlayIcon} />
                 <HeaderIcon Icon={ShoppingCartIcon} />
@@ -53,7 +54,15 @@ function Header() {
             </div>
         </div>
         {/* RIGHT */}
+        <div className="flex items-center sm:space-x-2 justify-end">
+            {/* ProfilePic */}
 
+            <p className='whitespace-nowrap font-seibold pr-3'>Zach Cortez</p>
+            <ViewGridIcon className='icon' />
+            <ChatIcon className='icon' />
+            <BellIcon className='icon' />
+            <CheveronDownIcon className='icon' />
+        </div>
         </div>
     )
 }
