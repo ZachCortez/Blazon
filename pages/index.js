b6cbfb5a96935afd1d5a32c31954ef6e909ceba1
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-04-28 22:16:09 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { getSession } from "next-auth/client";
 import Head from 'next/head';
 import Header from "../components/Header";
+import Login from "../components/Login";
 
-export default function Home() {
+
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
@@ -15,7 +20,17 @@ export default function Home() {
       {/* Feed */}
       {/* Widgets */}
     </main>
+  </div>
+  );
+}
 
-    </div>
-  )
+export async function getServerSideProps(context) {
+  // Get the User
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
